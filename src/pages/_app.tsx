a3900f0ae82cf0fa60dee8aa751fed18fff5ace8
Tom Wiesing <tkw01536@gmail.com>
2022-01-22 10:06:05 +0100

    material-ui: Explicitly use default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,13 @@
 import '@fontsource/roboto';
 
 import App from "next/app";
-export default App;
+
+import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core/styles';
+
+const defaultTheme = createTheme({});
+export default class extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+        return <ThemeProvider theme={defaultTheme}><Component {...pageProps} /></ThemeProvider>;
+    }
+}
