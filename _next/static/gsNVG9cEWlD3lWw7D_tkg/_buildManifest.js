16a8c9f0589b6985171f26db48db0dd486b2fb9a
GitHub Action <tkw1536@users.noreply.github.com>
2023-10-26 18:58:33 +0000

    6166d1a5daa442f1116579660035a0b10a925764

---
+++ _next/static/gsNVG9cEWlD3lWw7D_tkg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/849-2c89085e140e8cc9.js","static/css/8cad206875541e20.css","static/chunks/pages/index-e6a4f424de52dc0b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
