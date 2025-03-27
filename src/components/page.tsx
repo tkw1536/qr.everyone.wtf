55dc8d0acacc55689a0e4e193e7b6b7f2aa19ee9
Tom Wiesing <tkw01536@gmail.com>
2025-03-27 20:11:55 +0100

    update dependencies

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Head from 'next/head';
 
-export default class Page extends React.Component<{title: string, url: string, description: string, image?: string}> {
+export default class Page extends React.Component<{title: string, url: string, description: string, image?: string, children?: React.ReactNode}> {
   render() {
     const {
       title, url, description, image, children,
